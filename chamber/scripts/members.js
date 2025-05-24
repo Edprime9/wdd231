cc55087e333a8c00fad17d7228628c8e93d43907
Edprime9 <meli@byupathway.edu>
2025-05-24 13:17:16 -0700

    week02

---
+++ chamber/scripts/members.js
@@ -0,0 +1,47 @@
+const memberContainer = document.getElementById("members");
+const gridBtn = document.getElementById("grid-view");
+const listBtn = document.getElementById("list-view");
+
+async function getMembers() {
+  const response = await fetch("data/members.json");
+  const data = await response.json();
+  displayMembers(data);
+}
+
+function displayMembers(members) {
+  memberContainer.innerHTML = "";
+  members.forEach((member) => {
+    const card = document.createElement("section");
+    card.classList.add("member-card");
+
+    card.innerHTML = `
+      <img src="images/${member.image}" alt="${member.name} logo" loading="lazy" />
+      <h3>${member.name}</h3>
+      <p>${member.address}</p>
+      <p><strong>Phone:</strong> ${member.phone}</p>
+      <p><strong>Website:</strong> <a href="${member.url}" target="_blank">${member.url}</a></p>
+    `;
+
+    memberContainer.appendChild(card);
+  });
+}
+
+const toggleButton = document.getElementById("menu-toggle");
+const navList = document.querySelector("#main-nav ul");
+
+toggleButton.addEventListener("click", () => {
+  navList.classList.toggle("show");
+});
+
+// View toggle functionality
+gridBtn.addEventListener("click", () => {
+  memberContainer.classList.add("grid-view");
+  memberContainer.classList.remove("list-view");
+});
+
+listBtn.addEventListener("click", () => {
+  memberContainer.classList.add("list-view");
+  memberContainer.classList.remove("grid-view");
+});
+
+getMembers();
